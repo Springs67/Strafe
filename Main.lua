129e6675ecd9ff775fa4ab3677bf0625f1ec222c
Spring67#0001 <109059103+Springs67@users.noreply.github.com>
2026-02-22 12:41:11 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,59 @@
+local base = 'https://raw.githubusercontent.com/Springs67/Strafe/refs/heads/Main/'
+local function getContents(link: string)
+    print(link)
+    local suc, ret = pcall(function()
+        return game:HttpGet(base .. link)
+    end)
+
+    return suc and ret or 'print("failed to recieve contents of "'..link..')'
+end
+local function install(file: string)
+    if not isfile(file) then
+        local oldFile = file
+        file = file:gsub('Strafe/', '')
+
+        writefile(oldFile, getContents(file))
+
+        repeat task.wait() until isfile(oldFile)
+
+        return readfile(oldFile)
+    end
+
+    return readfile(file)
+end
+
+if not isfolder('Strafe') then
+    makefolder('Strafe')
+    makefolder('Strafe/Games')
+    makefolder('Strafe/Configs')
+    makefolder('Strafe/Libraries')
+    makefolder('Strafe/Libraries/Bedwars')
+    makefolder('Strafe/Libraries/BedwarZ')
+    makefolder('Strafe/Libraries/BridgeDuels')
+end
+
+local Contents = {
+    'Games',
+    'Universal',
+    'GuiLibrary',
+    'Libraries/Entity',
+    'Libraries/Network',
+    'Libraries/FakeDamage',
+    'Libraries/ProgressBar',
+    'Libraries/BridgeDuels/Meta',
+
+    'Games/BedwarZ',
+    'Games/BridgeDuels',
+}
+
+for _, v in Contents do
+    if not isfile('Strafe/'..v..'.lua') then
+        install('Strafe/'..v..'.lua')
+    end
+end
+
+--return loadstring(install('Strafe/Main.lua'))()
+
 local GuiLibrary = loadfile('Strafe/GuiLibrary.lua')()
 local Games = loadfile('Strafe/Games.lua')()
 loadfile('Strafe/Universal.lua')()
